8994fd7ed5fc2312d2caa756d132085eb210bb6d
unknown <nguyenvietlong2803@gmail.com>
2021-06-04 11:12:49 +0700

    second

--- nodejs_blog-master/src/routes/site.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const route = require('.');
-const router = express.Router();
-const siteController = require('../app/controllers/SiteController');
-
-router.get('/search', siteController.search);
-router.get('/', siteController.index);
-
-module.exports = router;
