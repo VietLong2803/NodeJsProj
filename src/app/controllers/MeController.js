6c8e9e5d237953a3ded4060899c5dcff71d6d931
unknown <nguyenvietlong2803@gmail.com>
2021-06-04 10:51:34 +0700

    first

---
+++ src/app/controllers/MeController.js
@@ -0,0 +1,26 @@
+const { json } = require('express');
+const Course = require('../models/Course')
+const { mutipleMongooseToObject } = require('../../util/mongoose');
+class MeController {
+
+    //[GET]/me/stored/courses
+    storedCourses(req, res, next) {
+
+            Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+                .then(([courses, deleteCount]) =>
+                    res.render('me/stored-courses', {
+                        deleteCount,
+                        courses: mutipleMongooseToObject(courses)
+                    })
+                ).catch(next)
+        }
+        //[GET]/me/trast/courses
+    trashCourses(req, res, next) {
+        Course.findDeleted({})
+            .then(courses => res.render('me/trash-courses', {
+                courses: mutipleMongooseToObject(courses)
+            }))
+            .catch(next)
+    }
+}
+module.exports = new MeController();
